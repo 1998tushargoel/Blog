bfedc7f6a41a1d2780abbf70154e7be91ab66465
1998tushargoel <1998tushargoel@gmail.com>
2019-06-11 12:52:16 +0530

    edit and update routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express    = require("express"),
+    methodOverride = require("method-override"),
     app        = express();
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose");
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser : tru
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -66,6 +68,28 @@ app.get("/blogs/:id", function(req, res){
             res.render("show", {blog: foundBlog});
         }
     })
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog : foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatesBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
 });
 
 app.listen(3000, function(){
